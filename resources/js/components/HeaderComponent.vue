4042b3b1db68100f214680dfdf0d224630794bcc
藤井麟太郎 <atarimaetaisou@icloud.com>
2020-08-18 11:46:20 +0900

    create header component

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container^fruid bg-dark mb-3">
+        <div class="container">
+            <nav class="navbar navbar-dark">
+                <span class="navbar-brand mb-0 h1">Vue Laravel SPA</span>
+                <div>
+                <router-link v-bind:to="{name: 'task.list'}">
+                    <button class="btn btn-success">List</button>
+                </router-link>
+                <button class="btn btn-success">Add</button>
+                </div>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
